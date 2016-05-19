ddf0cc9e414b63b5562d3d3daf4d6139747e0614
Jorge Vergara <javebratt@gmail.com>
2016-05-19 00:59:55 -0500

    ngCordova Added ngCordova to use the translate and the admob plugins

---
+++ www/lib/admob.js
@@ -0,0 +1,43 @@
+var admobid = {};
+if( /(android)/i.test(navigator.userAgent) ) {
+    admobid = { // for Android
+        banner: 'ca-app-pub-8142829781299976/6101515649',
+        interstitial: 'ca-app-pub-8142829781299976/7578248843'
+    };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+    admobid = { // for iOS
+        banner: '',
+        interstitial: ''
+    };
+} else {
+    admobid = { // for Windows Phone
+        banner: '',
+        interstitial: ''
+    };
+}
+
+if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+    document.addEventListener('deviceready', initApp, false);
+} else {
+    initApp();
+}
+
+function initApp() {
+    if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
+
+    AdMob.createBanner( {
+        adId: admobid.banner,
+        isTesting: true,
+        overlap: false,
+        offsetTopBar: false,
+        position: AdMob.AD_POSITION.BOTTOM_CENTER,
+        autoShow: true,
+        bgColor: 'black'
+    } );
+
+    AdMob.prepareInterstitial({
+        adId: admobid.interstitial,
+        isTesting: true,
+        autoShow: false
+    });
+}
