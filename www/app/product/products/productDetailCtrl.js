02a4ac856b4a07bdb22026d89806e94dfbebdb60
Jorge Vergara <javebratt@gmail.com>
2016-05-19 12:43:19 -0500

    Share Product Integrated  to let users share their products with potential customers

--- www/app/product/products/productDetailCtrl.js
+++ www/app/product/products/productDetailCtrl.js
@@ -5,12 +5,35 @@
       .module('app.product')
       .controller('ProductDetailCtrl', ProductDetailCtrl);
 
-  ProductDetailCtrl.$inject = ['$scope', 'user', 'ProductService', '$state', '$ionicHistory'];
+  ProductDetailCtrl.$inject = ['$scope', 'user', 'ProductService', '$state', '$cordovaSocialSharing'];
 
-  function ProductDetailCtrl($scope, user, ProductService, $state, $ionicHistory){
+  function ProductDetailCtrl($scope, user, ProductService, $state, $cordovaSocialSharing){
 
     $scope.currentProduct = ProductService.productDetail(user.$id, $state.params.pId);
 
+    $scope.shareWhatsapp = function(){
+      var message = $scope.currentProduct.name + '\n\n' + $scope.currentProduct.description + '\n\nPrice: $' +
+        $scope.currentProduct.price;
+
+      $cordovaSocialSharing.shareViaWhatsApp(message, null, null)
+        .then(function(result) {
+          if (window.AdMob) AdMob.showInterstitial();
+        }, function(err) {
+          // An error occurred. Show a message to the user
+        });
+    };
+
+    $scope.shareEmail = function(){
+      var message = $scope.currentProduct.name + '\n\n' + $scope.currentProduct.description + '\n\nPrice: $' +
+        $scope.currentProduct.price;
+
+      $cordovaSocialSharing.shareViaEmail(message, 'Info about ' + $scope.currentProduct.name, null, null, null, null)
+        .then(function(result) {
+          if (window.AdMob) AdMob.showInterstitial();
+        }, function(err) {
+          // An error occurred. Show a message to the user
+        });
+    };
 
   };
 })();
